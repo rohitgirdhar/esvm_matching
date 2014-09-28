238daa6fc2af3392d8a21c5b5b8ba8b00e83ed63
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2014-09-28 02:06:48 -0400

    clip regions

--- esvm_run_eval_clip.m
+++ esvm_run_eval_clip.m
@@ -1,15 +1,15 @@
-function esvm_run_eval(imgsDir, trainFilesListFpath, ...
+function esvm_run_eval_clip(imgsDir, trainFilesListFpath, ...
         testFilesListFpath, negFolder)
 % imgsDir : path to directory with all images
 % testFilesListFpath : path to file with test images, wrt imgsDir
 % trainFilesListFpath : path to file with train images, wrt imgsDir
 
 % Use this folder to sync the running of multiple instances of this func
-SYNC_FOLDER = 'run-sync/';
+SYNC_FOLDER = 'run_syncs/sync_people_occ2/';
 if ~exist(SYNC_FOLDER, 'dir')
     mkdir(SYNC_FOLDER);
 end
-CACHE_DIR = 'models-cache';
+CACHE_DIR = 'caches/models-people_occ2';
 if ~exist(CACHE_DIR, 'dir')
     mkdir(CACHE_DIR);
 end
@@ -36,6 +36,7 @@ for i = 1 : numel(testFilesList)
     % to remove lock file in case of forced exit or kill
     cleanupObj = onCleanup(@() rmdir(fullfile(SYNC_FOLDER, [test_hash, '.lock'])));
 
+    masks_dir = fullfile(imgsDir, '../', 'masks');
     try
         %% actual computation
         I = imread(fullfile(imgsDir, testFpath));
@@ -46,18 +47,20 @@ for i = 1 : numel(testFilesList)
             load(model_cache_path);
             fprintf('Read the model from file: %s\n', model_cache_path);
         else
-            for mask_id = 1 : 3
-                M = genScaledMask(size(I, 2), size(I, 1), mask_id - 1);
-                models = esvm_train_single_exemplar(I, ...
-                    [1 1 size(I, 2) size(I, 1)], negFolder, ...
-                    [img_id, '_', num2str(mask_id)], 'mask_img', M);
-             %   save(model_cache_path, 'models');
-                out_dir = ['res/www/corpus.', img_id, '_', num2str(mask_id), '-svm'];
-                mkdir(out_dir);
-                imwrite(M, fullfile(out_dir, 'mask.jpg'));
-                esvm_get_closest_matches(models, imgsDir, trainFilesListFpath, 20);
-            end
+
+            [t_path, t_fname, t_ext] = fileparts(testFpath);
+            M = im2bw(imread(fullfile(masks_dir, ...
+                            t_path, ['mask_', t_fname, t_ext])));
+            models = esvm_train_single_exemplar(I, ...
+                [1 1 size(I, 2) size(I, 1)], negFolder, ...
+                img_id, 'mask_img', M);
+            save(model_cache_path, 'models');
+            out_dir = ['results/res_people_occ2/www/corpus.', img_id, '-svm'];
+            mkdir(out_dir);
+            imwrite(M, fullfile(out_dir, 'mask.jpg'));
         end
+        esvm_get_closest_matches(models, imgsDir, trainFilesListFpath, ...
+                21, 'res_folder', 'results/res_people_occ2');
         mkdir(fullfile(SYNC_FOLDER, [test_hash, '.done']));
     catch
     end
