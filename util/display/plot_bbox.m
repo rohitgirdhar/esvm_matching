1676cc5eeb6dcfa0035e0cacd7e037a1658f388f
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2014-10-04 13:30:24 -0400

    fixed large fonts

--- util/display/plot_bbox.m
+++ util/display/plot_bbox.m
@@ -97,5 +97,5 @@ if exist('titler','var') & length(titler)>0
   hold on;
   text(bb(1),bb(2),titler,'color','k','backgroundcolor',col1,...
        'verticalalignment','bottom','horizontalalignment','left', ...
-       'fontsize',min(50,max(10,round(fontscale))));
+       'fontsize',min(20,max(10,round(fontscale))));
 end
